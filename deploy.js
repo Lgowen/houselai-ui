7e22c7450b172e1729f79a56bc39e7a8c1f61230
Lgowen <434666361@qq.com>
2023-07-24 01:22:21 +0800

    test: 自动化脚本

--- deploy.js
+++ deploy.js
@@ -4,8 +4,6 @@ const APP_ID = process.env.APP_ID
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const TOOL_ID = process.env.TOOL_ID
 
-console.log('APP_ID', APP_ID, process.env)
-
 async function setInfo() {
     await minidev.config.useRuntime({
         'alipay.authentication.privateKey': PRIVATE_KEY,
@@ -17,7 +15,7 @@ const fs = require('fs');
 
 const dir = 'dist/build/mp-alipay';
 
-if (fs.existsSync(dir)) {
+if (fs.readdirSync(dir)) {
   console.log(`${dir} exists`);
 } else {
   console.log(`${dir} does not exist`);
@@ -28,7 +26,7 @@ async function upload() {
     minidev.upload({
         appId: APP_ID, // (对应appid)
         // identityKeyPath: './config.json', // 身份验证文件
-        project: 'dist/build/mp-alipay', // uniapp打包后的路径
+        project: './dist/build/mp-alipay', // uniapp打包后的路径
         experience: true // 是否设置为体验版
       })
       .then(() => {
