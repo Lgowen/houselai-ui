ff3ca9648d60e7f97951f37664ed5743de2b932e
Lgowen <434666361@qq.com>
2023-07-24 01:33:07 +0800

    test: 自动化脚本

--- deploy.js
+++ deploy.js
@@ -4,6 +4,14 @@ const APP_ID = process.env.APP_ID
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const TOOL_ID = process.env.TOOL_ID
 
+// 获取工作目录路径
+const workspacePath = process.env.GITHUB_WORKSPACE;
+
+// 构建产物路径
+const buildPath = path.join(workspacePath, 'dist/build/mp-alipay');
+
+console.log(buildPath, 'buildPath')
+
 async function setInfo() {
     await minidev.config.useRuntime({
         'alipay.authentication.privateKey': PRIVATE_KEY,
@@ -11,20 +19,12 @@ async function setInfo() {
       });
 }
 
-const fs = require('fs');
-
-const dir = 'dist/build/mp-alipay';
-
-const res = fs.readdirSync(dir)
-
-console.log(res, 'res')
-
 async function upload() {
     await setInfo()
     minidev.upload({
         appId: APP_ID, // (对应appid)
         // identityKeyPath: './config.json', // 身份验证文件
-        project: './dist/build/mp-alipay', // uniapp打包后的路径
+        project: buildPath, // uniapp打包后的路径
         experience: true // 是否设置为体验版
       })
       .then(() => {
