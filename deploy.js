d9dab8dc9514790154d3228b68f1978e1c0b3080
Lgowen <434666361@qq.com>
2023-07-24 02:19:26 +0800

    test: 自动化脚本

--- deploy.js
+++ deploy.js
@@ -1,18 +1,16 @@
 const { minidev } = require('minidev')
 
-const path = require('path')
-
-const APP_ID = process.env.APP_ID 
+// const APP_ID = process.env.APP_ID || '2021004105633109'
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const TOOL_ID = process.env.TOOL_ID
 
 // 获取工作目录路径
-const workspacePath = process.env.GITHUB_WORKSPACE;
+const workspacePath = process.env.GITHUB_WORKSPACE
 
 // 构建产物路径
 const buildPath = path.join(workspacePath, 'dist/build/mp-alipay');
 
-console.log(buildPath, 'buildPath1')
+// console.log(buildPath, 'buildPath1')
 
 async function setInfo() {
     await minidev.config.useRuntime({
@@ -24,7 +22,7 @@ async function setInfo() {
 async function upload() {
     await setInfo()
     minidev.upload({
-        appId: APP_ID, // (对应appid)
+        appId: '2021004105633109', // (对应appid)
         // identityKeyPath: './config.json', // 身份验证文件
         project: buildPath, // uniapp打包后的路径
         experience: true // 是否设置为体验版
