6eff78eb55a0dc877b4c884c01d6f57ffd668dc7
Lgowen <434666361@qq.com>
2023-07-24 00:05:06 +0800

    test: 自动化脚本

--- deploy.js
+++ deploy.js
@@ -1,33 +1,31 @@
-// const { minidev } = require('minidev')
+const { minidev } = require('minidev')
 
 const APP_ID = process.env.APP_ID
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const TOOL_ID = process.env.TOOL_ID
 
-// console.log('process.env', process.env)
-console.log('APP_ID', APP_ID, 'PRIVATE_KEY', PRIVATE_KEY, 'TOOL_ID1', TOOL_ID)
-// async function setInfo() {
-//     await minidev.config.useRuntime({
-//         'alipay.authentication.privateKey': PRIVATE_KEY,
-//         'alipay.authentication.toolId': TOOL_ID,
-//       });
-// }
+async function setInfo() {
+    await minidev.config.useRuntime({
+        'alipay.authentication.privateKey': PRIVATE_KEY,
+        'alipay.authentication.toolId': TOOL_ID,
+      });
+}
 
-// async function upload() {
-//     await setInfo()
-//     minidev.upload({
-//         appId: APP_ID, // (对应appid)
-//         // identityKeyPath: './config.json', // 身份验证文件
-//         project: 'dist/build/mp-alipay', // uniapp打包后的路径
-//         experience: true // 是否设置为体验版
-//       })
-//       .then(() => {
-//         console.log('上传成功！')
-//       })
-//       .catch((error) => {
-//         console.log('上传失败，原因：', error)
-//         process.exit(-1)
-//       })
-// }
+async function upload() {
+    await setInfo()
+    minidev.upload({
+        appId: APP_ID, // (对应appid)
+        // identityKeyPath: './config.json', // 身份验证文件
+        project: 'dist/build/mp-alipay', // uniapp打包后的路径
+        experience: true // 是否设置为体验版
+      })
+      .then(() => {
+        console.log('上传成功！')
+      })
+      .catch((error) => {
+        console.log('上传失败，原因：', error)
+        process.exit(-1)
+      })
+}
 
-// upload()
+upload()
