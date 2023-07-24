4140979a42413cfe313ccc34508e1f8d61450d5d
Lgowen <434666361@qq.com>
2023-07-24 14:09:01 +0800

    test: auto upload

--- deploy.js
+++ deploy.js
@@ -3,21 +3,21 @@ const { minidev } = require('minidev')
 const path = require('path')
 
 const APP_ID = process.env.APP_ID
-const PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY , 'base64').toString() 
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
 const TOOL_ID = process.env.TOOL_ID
 
-// const fs = require('fs');
+const fs = require('fs');
 
 // 读取 JSON 文件
-// const config = fs.readFileSync('./config.json', 'utf8');
+const config = fs.readFileSync('./config.json', 'utf8');
 
-// // 替换占位符
-// const replacedConfig = config
-//   .replace('secrets.TOOL_ID', process.env.TOOL_ID)
-//   .replace('secrets.PRIVATE_KEY', process.env.PRIVATE_KEY);
+// 替换占位符
+const replacedConfig = config
+  .replace('secrets.TOOL_ID', TOOL_ID)
+  .replace('secrets.PRIVATE_KEY', PRIVATE_KEY);
 
 // 将替换后的内容写回到 JSON 文件 
-// fs.writeFileSync('./config.json', replacedConfig, 'utf8');
+fs.writeFileSync('./config.json', replacedConfig, 'utf8');
 
 // 获取工作目录路径
 const workspacePath = process.env.GITHUB_WORKSPACE
@@ -27,18 +27,18 @@ const buildPath = path.join(workspacePath, 'dist/build/mp-alipay');
 
 // console.log(buildPath, 'buildPath1')
 
-async function setInfo() {
-    await minidev.config.useRuntime({
-        'alipay.authentication.privateKey': PRIVATE_KEY,
-        'alipay.authentication.toolId': TOOL_ID,
-      });
-}
+// async function setInfo() {
+//     await minidev.config.useRuntime({
+//         'alipay.authentication.privateKey': PRIVATE_KEY,
+//         'alipay.authentication.toolId': TOOL_ID,
+//       });
+// }
 
 async function upload() {
-    await setInfo()
+    // await setInfo()
     minidev.upload({
         appId: APP_ID, // (对应appid)
-        // identityKeyPath: './config.json', // 身份验证文件
+        identityKeyPath: './config.json', // 身份验证文件
         project: buildPath, // uniapp打包后的路径
         experience: true // 是否设置为体验版
       })
